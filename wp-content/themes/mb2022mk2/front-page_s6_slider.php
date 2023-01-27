93772da8725caaf995c8d1aa3fd62c7494410cc9
Dymirt <kolidadmytro@gmail.com>
2023-01-27 20:59:02 +0100

    fixing slide issue on last section

--- wp-content/themes/mb2022mk2/front-page_s6_slider.php
+++ wp-content/themes/mb2022mk2/front-page_s6_slider.php
@@ -1,35 +1,38 @@
-<div id="suits" class="carousel slide" data-ride="carousel" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-center">
-  <ol class="carousel-indicators d-none">
-    <li data-target="#suits" data-slide-to="0" class="active"></li>
-    <li data-target="#suits" data-slide-to="1"></li>
-    <li data-target="#suits" data-slide-to="2"></li>
-    <li data-target="#suits" data-slide-to="3"></li>
-  </ol>
-  <div class="carousel-inner">
-  <div class="overlay caption d-flex d-sm-none">
+<div id="suits" class="carousel slide" data-aos="fade-up" data-aos-duration="1000"
+     data-aos-anchor-placement="top-center">
+    <ol class="carousel-indicators">
+        <li data-bs-target="#suits" data-bs-slide-to="0" class="active"></li>
+        <li data-bs-target="#suits" data-bs-slide-to="1"></li>
+        <li data-bs-target="#suits" data-bs-slide-to="2"></li>
+        <li data-bs-target="#suits" data-bs-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="overlay caption d-flex d-sm-none">
             <h1>Suits</h1>
-            <a href="sklep/kategoria/garnitury"><button class="black">Zobacz więcej</button></a>
+            <a href="sklep/kategoria/garnitury">
+                <button class="black">Zobacz więcej</button>
+            </a>
         </div>
-    <div class="carousel-item active">
-      <img class="d-block w-100"  src="<? echo get_stylesheet_directory_uri()?>/img/s6/1s6.jpg">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="<? echo get_stylesheet_directory_uri() ?>/img/s6/1s6.jpg">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<? echo get_stylesheet_directory_uri() ?>/img/s6/2s6.jpg">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<? echo get_stylesheet_directory_uri() ?>/img/s6/4s6.jpg">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<? echo get_stylesheet_directory_uri() ?>/img/s6/5s6.jpg">
+        </div>
+    </div>
 
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100"  src="<? echo get_stylesheet_directory_uri()?>/img/s6/2s6.jpg">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100"  src="<? echo get_stylesheet_directory_uri()?>/img/s6/4s6.jpg">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100"   src="<? echo get_stylesheet_directory_uri()?>/img/s6/5s6.jpg">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#suits" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#suits" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <a class="carousel-control-prev" type="button" data-bs-target="#suits" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </a>
+    <a class="carousel-control-next" type="button" data-bs-target="#suits" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </a>
 </div>
