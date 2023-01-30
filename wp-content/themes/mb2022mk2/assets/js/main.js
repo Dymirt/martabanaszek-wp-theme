948b08db1a3777f98b61fa6a1ecc7c0d499fa809
Dymirt <kolidadmytro@gmail.com>
2023-01-30 10:08:32 +0100

    adding size table to product page

--- wp-content/themes/mb2022mk2/assets/js/main.js
+++ wp-content/themes/mb2022mk2/assets/js/main.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (document.body.classList.contains('single-product')) {
+        insertProductSizeModalButton();
+    }
+});
+
 // cookies-info
 function cookiesClose(){
     document.getElementById('cookies-info').style.display = 'none';
@@ -5,4 +11,10 @@ function cookiesClose(){
 function cookiesAlertInfo() {
     alert("Strona korzysta z plików cookie. Wykorzystywane są one m.in. do zapamiętywania ustawień oraz zbierania danych statystycznych. Jeśli nie chcesz korzystać z plików cookie, proszę opuść tę stronę.");
 }
-// End cookies-info
+
+// Adding Size table button on product page
+function insertProductSizeModalButton(){
+    let elem = document.getElementsByClassName('woocommerce-product-details__short-description');
+    elem.item(0).innerHTML += "<a id='sizeTableButton'>Tabela rozmiarów</a>";
+}
+
