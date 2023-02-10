e477b65fb9a49b454f2de074405ae790ee35816e
Dymirt <kolidadmytro@gmail.com>
2023-02-10 22:14:23 +0100

    styling article

--- wp-content/themes/mb2022mk2/template-parts/content-partners.php
+++ wp-content/themes/mb2022mk2/template-parts/content-partners.php
@@ -3,7 +3,7 @@ global $post;
 $partner_url = get_post_meta($post->ID, 'partner_url', true);
 if (has_post_thumbnail()) { ?>
     <div class="col">
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="margin-bottom: 0">
             <a href="<?php echo $partner_url; ?>">
                 <img src="<?php the_post_thumbnail_url(); ?>"
                      style="object-fit: contain; aspect-ratio: 1/1; width: 100%;">
