a81e8394fe659060b10df6bf368ece5e0584f3f2
Dymirt <kolidadmytro@gmail.com>
2023-02-28 20:12:42 +0100

    changing style

--- wp-content/themes/mb2022mk2/archive-partners.php
+++ wp-content/themes/mb2022mk2/archive-partners.php
@@ -5,19 +5,34 @@
  */
 
 get_header(); ?>
+
+<style>
+    body{
+        background-color: black;
+    }
+</style>
 <?php do_action('menu'); ?>
     <div class="content-area-wrapper">
     <div id="primary" class="content-area partners">
-        <main id="main" class="site-main" role="main" style="margin-top: 80px">
-            <h1 class="page-title" style="color: white; background-color: black; margin-bottom: 0">DOTYCZCZAS ZAUFALI NAM</h1>
-            <div class="row" style="background-color: black">
-                <?php
-                while (have_posts()) :
-                    the_post();
-                    do_action('storefront_page_before');
-                    get_template_part('template-parts/content', 'partners');
-                endwhile; // End of the loop.
-                ?>
+        <main id="main" class="site-main" role="main">
+            <div class="container">
+                <h1 class="page-title" style="color: white;">DOTYCZCZAS ZAUFALI NAM</h1>
+                <div class="row">
+                    <?php
+                    $args = array(
+                            'post_type' => 'partners',
+                            'orderby' => 'rand',
+                            'posts_per_page' => 100,
+                    );
+                    $query = new WP_Query( $args );
+
+                    while ($query->have_posts()) :
+                        $query->the_post();
+                        do_action('storefront_page_before');
+                        get_template_part('template-parts/content', 'partners');
+                    endwhile; // End of the loop.
+                    ?>
+                </div>
             </div>
         </main><!-- #main -->
     </div><!-- #primary -->
